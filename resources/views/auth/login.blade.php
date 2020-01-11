77fb62a15fa9e1d43170a0b7cb65f7cc9cb8296f
camara <camara>
2020-01-11 20:41:01 +0000

    creation user vu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,75 +2,13 @@
 
 @section('contenu_de_la_page')
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
-<!DOCTYPE html>
-<html>
-    
-<head>
-	<title>My Awesome Login Page</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
-</head>
-<!--Coded with love by Mutiullah Samim-->
-<body>
-	<div class="container h-100">
-		<div class="d-flex justify-content-center h-100">
-			<div class="user_card">
-				<div class="d-flex justify-content-center">
-					<div class="brand_logo_container">
-						<img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-circle-logo-png-transparent.png" class="brand_logo" alt="Logo">
-					</div>
-				</div>
-				<div class="d-flex justify-content-center form_container">
-					<form>
-						<div class="input-group mb-3">
-							<div class="input-group-append">
-								<span class="input-group-text"><i class="fas fa-user"></i></span>
-							</div>
-							<input type="text" name="" class="form-control input_user" value="" placeholder="username">
-						</div>
-						<div class="input-group mb-2">
-							<div class="input-group-append">
-								<span class="input-group-text"><i class="fas fa-key"></i></span>
-							</div>
-							<input type="password" name="" class="form-control input_pass" value="" placeholder="password">
-						</div>
-						<div class="form-group">
-							<div class="custom-control custom-checkbox">
-								<input type="checkbox" class="custom-control-input" id="customControlInline">
-								<label class="custom-control-label" for="customControlInline">Remember me</label>
-							</div>
-						</div>
-							<div class="d-flex justify-content-center mt-3 login_container">
-				 	<button type="button" name="button" class="btn login_btn">Login</button>
-				   </div>
-					</form>
-				</div>
-		
-				<div class="mt-4">
-					<div class="d-flex justify-content-center links">
-						Don't have an account? <a href="#" class="ml-2">Sign Up</a>
-					</div>
-					<div class="d-flex justify-content-center links">
-						<a href="#">Forgot your password?</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
 
 
 
-<!-- <div class="container">
+
+<div class="container">
     <div class="row justify-content-rigth">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Connexion') }}</div>
 
@@ -136,36 +74,9 @@
             </div>
         </div>
     </div>
-</div>
 
-<div class="container bg-Pulple">
-<div class="spinner-border text-primary" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-secondary" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-success" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-danger" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-warning" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-info" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-light" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-dark" role="status">
-  <span class="sr-only">Loading...</span>
-</div>
-<h4 display="center">INSCRIPTION EST GRATUITE </h4>
-    <div class="row justify-content-left" >
-        <div class="col-md-8">
+<h4 display="center bg-success">INSCRIPTION EST GRATUITE </h4>
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Inscription') }}</div>
                 <div class="card-body">
@@ -275,6 +186,6 @@
     </div>
   
 
-</div> -->
+</div>
 
 @endsection
